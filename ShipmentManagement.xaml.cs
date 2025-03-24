08a24b95ce488ee321983b2b5aa3ecc8954f478f
jharashree <jhara1shree@gmail.com>
2025-03-24 07:23:25 -0400

    Add files via upload

---
+++ ShipmentManagement.xaml.cs
@@ -0,0 +1,77 @@
+﻿/// <summary>
+/// Interaction Properties for shipment management
+/// Created By: Jharashree Pattnaik
+/// Created On: 7-3-2025
+/// </summary>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Reflection.Emit;
+using System.Web;
+using ShipmentManagement.ViewModels;
+using System.Configuration;
+
+
+namespace ShipmentManagement
+{
+    /// <summary>
+    /// Interaction logic for ShipmentManagement.xaml
+    /// </summary>
+    public partial class ShipmentManagement : Window
+    {
+        public ShipmentManagement()
+        {
+            InitializeComponent();
+        }
+        ShimentManagementViewModel objshipment;
+        string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("usp_ShipmentManagement", con))
+                {
+                    try
+                    {
+                        con.Open();
+                        // create an instance of the class.
+                        objshipment = new ShimentManagementViewModel();
+                        objshipment.StrAction = "A";
+                        objshipment.strShipmenttype = Convert.ToString(ShipmentTypeComboBox.SelectedItem);
+                        objshipment.strOrigin = Convert.ToString(txtOrigin.Text);
+                        objshipment.strDestination = Convert.ToString(txtDestination.Text);
+                        objshipment.strDatetime = Convert.ToDateTime(txtDate.Text);
+                        objshipment.intStatus = Convert.ToInt32(StatusComboBox.SelectedValue);
+                        objshipment.strOutput = "1"; //Data saved successfully
+                        con.Close();
+                    }
+                    catch (Exception ex) //Exception handled
+                    {
+
+                        throw new ArgumentOutOfRangeException(
+                        "Error Occured.", ex);
+                        throw;
+                    }
+                    finally
+                    {
+                        objshipment = null;
+                    }
+          
+                }
+            }
+        }
+    }
+}
+    
+
---
+++ Viewshipment.xaml.cs
@@ -0,0 +1,90 @@
+﻿/// <summary>
+/// View details for shipment management
+/// Created By: Jharashree Pattnaik
+/// Created On: 7-3-2025
+/// </summary>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Reflection.Emit;
+using System.Web;
+using ShipmentManagement.ViewModels;
+using System.Configuration;
+using System.Collections.ObjectModel;
+
+namespace ShipmentManagement
+{
+    /// <summary>
+    /// Interaction logic for Viewshipment.xaml
+    /// </summary>
+    public partial class Viewshipment : Window
+    {
+        ShimentManagementViewModel objshipment = new ShimentManagementViewModel();
+        string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+
+        public Viewshipment()
+        {
+            //InitializeComponent();
+         
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("usp_ShipmentManagement", con))
+                {
+                    try
+                    {
+                        ObservableCollection<ShimentManagementViewModel> _shipmentCollection =
+                        new ObservableCollection<ShimentManagementViewModel>();
+
+
+                        _shipmentCollection.Add(new ShimentManagementViewModel
+                        {
+                            StrAction = "V",
+                            strShipmenttype = objshipment.strShipmenttype,
+                            strOrigin = objshipment.strOrigin,
+                            strDestination = objshipment.strDestination,
+                            intStatus = objshipment.intStatus
+
+                        });
+
+
+                        Viewshipment em = new Viewshipment();
+                        this.DataContext = em;
+
+
+                    }
+                    catch (Exception ex) //Exception handled
+                    {
+
+                        throw new ArgumentOutOfRangeException(
+                        "Error Occured.", ex);
+                        throw;
+                    }
+                    finally
+                    {
+                        objshipment = null;
+                    }
+       
+}
+
+            }
+        }
+
+        
+}
+}
+
+
+      
+
